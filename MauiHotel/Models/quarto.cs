78aa2b22a0c13165e637e464736e0fe8edae349d
Usuario <Usuario@CPD03-M06>
2024-06-10 15:59:31 -0300

    Adicionar arquivos de projeto.

---
+++ MauiHotel/Models/quarto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiHotel.Models
+{
+    internal class quarto
+    {
+        public string Descricao {  get; set; }
+        public double ValorDiariaAdulto {  get; set; }
+        public double ValorDiariaCrianca { get; set; }
+
+    }
+}
